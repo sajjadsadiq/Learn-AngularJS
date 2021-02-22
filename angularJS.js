5f129e522cc629de89924a428dd9f7f1aea7bd30
Sajjad Sadiq <sajjadsadiq2020@gmail.com>
2021-02-22 14:50:41 +0600

    Introduction AngularJS

---
+++ angularJS.js
@@ -0,0 +1,22 @@
+// Learn AngularJS
+/*
+AngularJS is perfect for Single Page Applications (SPAs).
+AngularJS is easy to learn.
+*/
+
+// 00
+/* 
+Introduction AngularJS
+A. AngularJS is a JavaScript framework. It can be added to an HTML page with a <script> tag.
+
+
+B. Directives
+
+
+C. Expressions
+
+
+D. AngularJS Extends HTML
+AngularJS extends HTML with ng-directives, ng-app(div elementer owner), ng-model(Bine variable contend), ng-bind(directive binds the content )
+
+*/
